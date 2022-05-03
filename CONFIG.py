9aed5dc51d30ea4a64578b8aeaddd68cc724e1eb
john <stormpapa135@gmail.com>
2022-05-03 19:08:07 -0400

    some constants into config.py

---
+++ CONFIG.py
@@ -0,0 +1,20 @@
+machine = 'merlin'
+# machine = 'batterylab'
+local = True
+
+
+if machine == 'merlin':
+    server = "dt-sql03"
+    db = "batterylab_finaltest"
+    table = "MERLIN"
+    report_folder_roots = ['C:\\PTM820eSystem\\Runtime\\Toyota Prius Battery_Gen2\\Test Sequence Logs\\',
+              'C:\\PTM820eSystem\\Runtime\\Toyota Prius Battery_Gen3\\Test Sequence Logs\\']
+    if local:
+        server = "JOHNLAPTOP\SQLEXPRESS"
+        report_folder_roots = ['C:\\work\\pyHarvest\\reports\\']
+
+else:
+    server = "JOHNLAPTOP\SQLEXPRESS"
+    # server = "dt-sql03"
+    db = "batterylab_finaltest"        
+    report_folder_roots = []
--- harvest.py
+++ harvest.py
@@ -3,27 +3,21 @@ import os
 import shutil
 from test_db_connect import read_table,connect_to_db
 from parse_merlin_report import parse_merlin
-
+import CONFIG
 
 def harvest_merlin():
-    server = 'JOHNLAPTOP\SQLEXPRESS'
-    db = 'batterylab_finaltest'
-    conn = connect_to_db(server,db)
-    table = "MERLIN"
-    df = read_table(table,conn)
-    # f_locs = ['C:\\work\\pyHarvest\\reports\\'
-    # f_locs = ['C:\\PTM820eSystem\\Runtime\\Toyota Prius Battery_Gen2\\Test Sequence Logs\\',
-            #   'C:\\PTM820eSystem\\Runtime\\Toyota Prius Battery_Gen3\\Test Sequence Logs\\']
-    f_locs = ['C:\\work\\pyHarvest\\reports\\']
-    for f_loc in f_locs:
-        directs = os.listdir(f_loc)  # 
-        directs = [f for f in directs if os.path.isdir(f_loc + f)]
-        print(f'{directs=}')
-        for direct in directs:
-            files = os.listdir(f_loc + "\\" + direct)
+
+    conn = connect_to_db(CONFIG.server,CONFIG.db)
+    data_from_db = read_table(CONFIG.table,conn)
+    for report_folder_root in CONFIG.report_folder_roots:
+        report_folders = os.listdir(report_folder_root)  # 
+        report_folders = [f for f in report_folders if os.path.isdir(report_folder_root + f)]
+        print(f'{report_folders=}')
+        for report_folder in report_folders:
+            files = os.listdir(report_folder_root + "\\" + report_folder)
             for file in files:
-                already_in_db = (file in df.FILE_NAME.values)
-                with open(f_loc + "\\" + direct + "\\" + file, encoding='latin-1') as f:
+                already_in_db = (file in data_from_db.FILE_NAME.values)
+                with open(report_folder_root + "\\" + report_folder + "\\" + file, encoding='latin-1') as f:
                     s = f.read(600)
                 is_report_file = s.find('START OF TEST') > 0
                 print(f'{file=},{already_in_db=},{is_report_file=}')
@@ -31,13 +25,13 @@ def harvest_merlin():
                     #mark so we don't try next time?
                     pass
                 elif is_report_file:
-                    data_to_db = parse_merlin(f_loc + direct + "\\",file)
+                    data_to_db = parse_merlin(report_folder_root + report_folder + "\\",file)
                     print(f"{data_to_db=}")
                     cursor = conn.cursor()
                     # Insert Dataframe into SQL Server:
-                    for index, row in data_to_db.iterrows():
+                    for _, row in data_to_db.iterrows():
                         sql=f"""
-                        INSERT INTO {table}
+                        INSERT INTO {CONFIG.table}
                         (time,FILE_NAME,PART_NUMBER,SERIAL_NUMBER,OVERALL_RESULT)
                         VALUES (?,?,?,?,?)
                         """
